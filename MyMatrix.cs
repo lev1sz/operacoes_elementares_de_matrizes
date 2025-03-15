d9910927fc085d7215903efe0b5cc360e2f7b0da
Levi <levilbeltrao1351@hotmail.com>
2025-03-15 20:36:36 -0300

    Initial commit: base do projeto

---
+++ MyMatrix.cs
@@ -0,0 +1,70 @@
+using System;
+class MyMatrix
+{
+    private int rows, cols;
+    private int[,] matrix;
+    public MyMatrix(int rows, int cols)
+    {
+        this.rows = rows;
+        this.cols = cols;
+        matrix = new int[rows, cols];
+    }
+    public void InsertNumbers()
+    {
+        for (int i = 0;i < this.rows; i++)
+        {
+            for (int j = 0;j < this.cols; j++)
+            {
+                Console.Write($"Insira um numero na celula {i+1}{j+1}: ");
+                matrix[i, j] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+    public void PrintMatrix()
+    {
+        for (int i = 0;i < this.rows; i++)
+        {
+            for (int j = 0;j < this.cols; j++)
+            {
+                Console.Write($"{matrix[i, j]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+    public void Permute(int i, int j)
+    {
+        int[] aux = new int[cols];
+        i--;
+        j--;
+        for(int k = 0; k < cols; k++)
+        {
+            aux[k] = matrix[i, k];
+        }
+        for(int k = 0; k < cols; k++)
+        {
+            matrix[i, k] = matrix[j, k];
+        }
+        for(int k = 0; k < cols; k++)
+        {
+            matrix[j, k] = aux[k];
+        }
+
+    }
+    public void Multiply(int i, int alpha)
+    {
+        i--;
+        for(int j = 0; j < cols; j++)
+        {
+            matrix[i, j] *= alpha;
+        }
+    }
+    public void MultiplyAndSum(int i, int j, int alpha)
+    {
+        i--;
+        j--;
+        for(int k = 0; k < cols; k++)
+        {
+            matrix[i, k] += matrix[j, k] * alpha;
+        }
+    }
+} 
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class Program{
+    static void Main(string[] args){
+        MyMatrix matriz = new MyMatrix(2,2);
+
+        matriz.InsertNumbers();
+        matriz.PrintMatrix();
+
+        matriz.Permute(1, 2);
+        Console.WriteLine("Matriz invertida:");
+        matriz.PrintMatrix();
+
+        matriz.Multiply(2, 3);
+        Console.WriteLine("Linha 2 multiplicada por 3:");
+        matriz.PrintMatrix();
+
+        matriz.MultiplyAndSum(1, 2, 2);
+        Console.WriteLine("Resultado da linha 1 somada pela linha 2 dobrada:");
+        matriz.PrintMatrix();
+    }
+}
